dccebad9dd154bb5e10ed75dc3795522a351a27e
sahan <sahan1989@gmail.com>
2019-07-10 10:45:28 +0200

    Test case 01 Changes

--- src/test/java/com/sahan/netent/controller/PreviousResultControllerTest.java
+++ src/test/java/com/sahan/netent/controller/PreviousResultControllerTest.java
@@ -21,7 +21,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class PreviousResultControllerTest {
 
     private static final int ID = 3;
-    private static final double AMOUNT = 20;
+    private static final int UNIQUE_ID = 100;
+    private static final double AMOUNT = 200.00;
 
     @Autowired
     private MockMvc mockMvc;
@@ -31,23 +32,23 @@ public class PreviousResultControllerTest {
 
     @Before
     public void setUp() {
-        when(service.findPreviousResultByUniqueId(ID)).thenReturn(createResultMock());
+        when(service.findPreviousResultByUniqueId(UNIQUE_ID)).thenReturn(createResultMock());
     }
 
     @Test
     public void testFindPreviousResult() throws Exception {
-        this.mockMvc.perform(get("/rest/resultController/result").param("uniqueId", String.valueOf(ID)))
+        this.mockMvc.perform(get("/rest/resultController/result").param("uniqueId", String.valueOf(UNIQUE_ID)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.amount", Matchers.is(AMOUNT)))
                 .andExpect(jsonPath("$.id", Matchers.is(ID)))
-                .andExpect(jsonPath("$.uniqueId",Matchers.is(ID)));
+                .andExpect(jsonPath("$.uniqueId",Matchers.is(UNIQUE_ID)));
     }
 
     private PreviousResult createResultMock() {
         PreviousResult result = new PreviousResult();
         result.setAmount(AMOUNT);
         result.setId(ID);
-        result.setUniqueId(ID);
+        result.setUniqueId(UNIQUE_ID);
         return result;
     }
 }
