3b2a1245a0c0e876972d4913a0cd1b886a85e418
sahan <sahan1989@gmail.com>
2019-07-10 13:02:04 +0200

    Unit Test 3

---
+++ src/test/java/com/sahan/netent/service/serviceImpl/PreviousResultServiceTest.java
@@ -0,0 +1,23 @@
+package com.sahan.netent.service.serviceImpl;
+
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PreviousResultServiceTest {
+    @BeforeClass
+    public static void init() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 8085;
+    }
+
+    @Test
+    public void testResultFetchesSuccess() {
+        get("rest/resultController/result?uniqueId=719")
+                .then()
+                .body("uniqueId", equalTo(719));
+    }
+}
